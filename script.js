f23e55371a78a596ef7ecd56d040bb4985d82d72
kavya-thakur <thakurkavya471@gmail.com>
2024-09-30 21:37:05 +0530

    final

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ ScrollTrigger.scrollerProxy("#main", {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
   getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
   },
   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
@@ -27,64 +27,88 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
 
-pageoneanimations = gsap.timeline()
-secoundpageanimation = gsap.timeline()
-
+let pageoneanimations = gsap.timeline()
+let secoundpageanimation = gsap.timeline()
 let mm = gsap.matchMedia();
-mm.add("(max-width:500px)" ,()=> {
-  //mobile
-  pageoneanimations.from(".hero-heading1 h1 , .hero-heading2 h1"  , {
-    y : "100%",
-    duration :0.6,
-    ease : Power4 ,
-    stagger : 0.4
+mm.add("(min-width:501px)", () => {
+  pageoneanimations.from(".hero-heading1 h1 , .hero-heading2 h1", {
+    y: "100%",
+    duration: 0.6,
+    ease: Power4,
+    stagger: 0.4
+  })
+  pageoneanimations.from(".svg", {
+    opacity: 0
+  })
+  pageoneanimations.from(".img", {
+    opacity: 0,
+  })
+  secoundpageanimation.from(".h2 h3", {
+    y: "100%",
+    duration: 0.6,
+    ease: Power4,
+    stagger: 0.4,
+    scrollTrigger:{
+      trigger: "#page2",
+      scroller: "#main",
+      // markers: true,
+      start: "top 40%",
+      end: "top 30%",
+      scrub: 2,
+    }
+  })
 })
-pageoneanimations.from(".svg",{
-  opacity : 0
+
+let thirdpageaniamtion = gsap.timeline()
+mm.add("(max-width:500px)", () => {
+  pageoneanimations.from(".hero-heading1 h1 , .hero-heading2 h1", {
+    y: "100%",
+    duration: 0.6,
+    ease: Power4,
+    stagger: 0.4
+  })
+  pageoneanimations.from(".svg", {
+    opacity: 0
+  })
+  pageoneanimations.from(".img", {
+    opacity: 0,
+  })
+  secoundpageanimation.from(".h2 h3", {
+    y: "100%",
+    duration: 0.6,
+    ease: Power4,
+    stagger: 0.4,
+    scrollTrigger:{
+      trigger: ".about",
+      scroller: "body",
+      markers: true,
+      start: "top 40%",
+      end: "top 30%",
+      scrub: 2,
+    }
+  })
 })
-pageoneanimations.from(".img",{
-  opacity : 0,
-})
-secoundpageanimation.from("#page2 .about , .about h3", {
+thirdpageaniamtion.from(".projectheading h1 , .projectheading i",{
   y : "100%" ,
-  duration :0.6,
-    ease : Power4 ,
-    stagger : 0.4 ,
-    scrollTrigger : {
-      trigger : ".secoundsection" ,
-      // scroller : ".secoundsection",
-      markers: true ,
-      start : "top 30%",
-      end : "top 20%",
-      scrub : 3 ,
+  scrollTrigger : {
+    trigger : ".projectheading h1",
+    scroller : "#main",
+    markers :true ,
+    start : "top 70%",
+    end : "top 40%",
+    scrub : 2 
   }
 })
+let fourpageanimation = gsap.timeline()
+
+fourpageanimation.from(".contactline h1" , {
+y : "100%" ,
+scrollTrigger : {
+  trigger: ".contactline",
+  markers: true,
+scroller: "#main",
+start : "top 70%",
+end: "top 40%" ,
+scrub:2
+}
 })
- mm.add("(min-width:501px)", ()=> {
-  pageoneanimations.from(".hero-heading1 h1 , .hero-heading2 h1"  , {
-    y : "100%",
-    duration :0.6,
-    ease : Power4 ,
-    stagger : 0.4
-})
-pageoneanimations.from(".svg",{
-  opacity : 0
-})
-pageoneanimations.from(".img",{
-  opacity : 0,
-})
-secoundpageanimation.from("#page2 .about , .about h3", {
-  y : "100%" ,
-  duration :0.6,
-    ease : Power4 ,
-    stagger : 0.4 ,
-    scrollTrigger : {
-      trigger : ".secoundsection" ,
-      scroller : "#main",
-      markers: true ,
-      start : "top 30%",
-      end : "top 20%",
-      scrub : 3 ,
-  }
-})
- })
